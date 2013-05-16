8ee250195971c464f96c74d4024ce59237d4ebc0
Riley Lynch <oss+ruby-lang@teleological.net>
2013-05-16 17:41:18 -0400

    Make Date#to_time symmetric with Time#to_date
    Time#to_date always interprets its month/day/year according to the Gregorian
    calender when constructing a Date, but Date#to_time uses the calendar
    of its start day. As a result, Time object returned for Julian dates
    have epoch second values which do not correspond to the Date object they
    were derived from, and if converted back to Dates with #to_date, yield
    a different date.

--- ext/date/date_core.c
+++ ext/date/date_core.c
@@ -8604,12 +8604,21 @@ time_to_datetime(VALUE self)
 static VALUE
 date_to_time(VALUE self)
 {
-    get_d1(self);
-
-    return f_local3(rb_cTime,
-		    m_real_year(dat),
-		    INT2FIX(m_mon(dat)),
-		    INT2FIX(m_mday(dat)));
+    get_d1a(self);
+    if (m_julian_p(adat)) {
+      VALUE tmp = d_lite_gregorian(self);
+      get_d1b(tmp);
+      return f_local3(rb_cTime,
+        m_real_year(bdat),
+        INT2FIX(m_mon(bdat)),
+        INT2FIX(m_mday(bdat)));
+    }
+    else {
+      return f_local3(rb_cTime,
+        m_real_year(adat),
+        INT2FIX(m_mon(adat)),
+        INT2FIX(m_mday(adat)));
+    }
 }
 
 /*
